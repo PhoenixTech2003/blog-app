7422c1bd2532cc714683ed09389c96596163d600
Chiyembekezo <phoenixtech2003@gmail.com>
2024-10-28 12:39:56 +0200

    Setup django compressor and tailwind css

--- blog-app-project/blogging_site/settings.py
+++ blog-app-project/blogging_site/settings.py
@@ -15,6 +15,17 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+COMPRESS_ROOT = BASE_DIR / 'blog_app/static/' 
+
+COMPRESS_ENABLED = True
+
+STATICFILES_FINDERS = [
+
+    'compressor.finders.CompressorFinder',
+]
+
+
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -37,6 +48,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'compressor',
+    'blog_app'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +67,7 @@ ROOT_URLCONF = 'blogging_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
---
+++ blog-app-project/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./blog_app/templates/blog_app/**/*.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
