4828ba3209278f3353141cc33da4496c017ecebb
Chiyembekezo <phoenixtech2003@gmail.com>
2024-10-30 11:10:07 +0200

    Add BlogPost model to index view

--- blog-app-project/blog_app/views.py
+++ blog-app-project/blog_app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import BlogPost
 
 # Create your views here.
 def index(request):
-    return render(request,"blog_app/index.html")
+    blog_posts = BlogPost.objects.all();
+    print(blog_posts)
+    return render(request,"blog_app/index.html", {"blogs": blog_posts})
