90d8e53670554d81bb4fb4bf1cf4dbf44524efff
Chiyembekezo <phoenixtech2003@gmail.com>
2024-10-31 12:15:52 +0200

    Add popular posts context to view

--- blog-app-project/blog_app/views.py
+++ blog-app-project/blog_app/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from .models import BlogPost
+from .models import BlogPost, Like
 
 # Create your views here.
 def index(request):
-    blog_posts = BlogPost.objects.all();
-    print(blog_posts)
-    return render(request,"blog_app/index.html", {"blogs": blog_posts})
+    blog_posts = BlogPost.objects.all()
+    popular_posts = BlogPost.top_liked_posts(BlogPost)
+    return render(request,"blog_app/index.html", {"blogs": blog_posts,"top_posts":popular_posts})
