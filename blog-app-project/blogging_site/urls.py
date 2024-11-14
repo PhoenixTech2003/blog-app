83eabb18e57b94720edfa49cff46f86cafca3695
Chiyembekezo <phoenixtech2003@gmail.com>
2024-11-14 07:10:43 +0200

    Add CKEditor 5 URL patterns to urlpatterns for media handling

--- blog-app-project/blogging_site/urls.py
+++ blog-app-project/blogging_site/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
 
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns +=[path("ckeditor5/",include('django_ckeditor_5.urls')),
+                   ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
