6d6f1c6217bd064da806f1cd0c08edd52f179ab4
Chiyembekezo <phoenixtech2003@gmail.com>
2024-11-14 07:09:08 +0200

    Add blog post detail view with error handling for non-existent posts

--- blog-app-project/blog_app/views.py
+++ blog-app-project/blog_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import BlogPost, Like
 
 # Create your views here.
@@ -6,3 +6,7 @@ def index(request):
     blog_posts = BlogPost.objects.all()
     popular_posts = BlogPost.top_liked_posts(BlogPost)
     return render(request,"blog_app/index.html", {"blogs": blog_posts,"top_posts":popular_posts})
+
+def blog_post(request, postId):
+    post = get_object_or_404(BlogPost, pk=postId)
+    return render(request,"blog_app/blogPost.html", {"post":post})
