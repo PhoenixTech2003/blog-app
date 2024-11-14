484c33d5ae824d280b1aabc85bf5e1577d59e3e3
Chiyembekezo <phoenixtech2003@gmail.com>
2024-11-14 07:10:31 +0200

    Add CKEditor 5 configuration and custom color palette to settings

--- blog-app-project/blogging_site/settings.py
+++ blog-app-project/blogging_site/settings.py
@@ -26,8 +26,77 @@ COMPRESS_ROOT = BASE_DIR / 'blog_app/static/'
 environ.Env.read_env(os.path.join(BASE_DIR,'.env'))
 COMPRESS_ENABLED = True
 
+STATIC_URL = '/static/'
+CKEDITOR_5_CUSTOM_CSS ='blog_app/ck-editor.css'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+customColorPalette = [
+        {
+            'color': 'hsl(4, 90%, 58%)',
+            'label': 'Red'
+        },
+        {
+            'color': 'hsl(340, 82%, 52%)',
+            'label': 'Pink'
+        },
+        {
+            'color': 'hsl(291, 64%, 42%)',
+            'label': 'Purple'
+        },
+        {
+            'color': 'hsl(262, 52%, 47%)',
+            'label': 'Deep Purple'
+        },
+        {
+            'color': 'hsl(231, 48%, 48%)',
+            'label': 'Indigo'
+        },
+        {
+            'color': 'hsl(207, 90%, 54%)',
+            'label': 'Blue'
+        },
+    ]
+CKEDITOR_5_CONFIGS = {
+    'default': {
+        'toolbar': ['heading', '|', 'bold', 'italic', 'link',
+                    'bulletedList', 'numberedList', 'blockQuote', 'imageUpload', ],
+        'width': 'auto',
+    
+
+    },
+    'extends': {
+        'blockToolbar': [
+            'paragraph', 'heading1', 'heading2', 'heading3',
+            '|',
+            'bulletedList', 'numberedList',
+            '|',
+            'blockQuote',
+        ],
+        'toolbar': ['heading', '|', 'outdent', 'indent', '|', 'bold', 'italic', 'link', 'underline', 'strikethrough','subscript', 'superscript', 'highlight', '|',  
+                    'bulletedList', 'numberedList', '|',  'blockQuote',  '|',
+                    'fontSize', 'fontFamily', 'fontColor', 'fontBackgroundColor',  'removeFormat',
+                    ],
+       
+        
+        'heading' : {
+            'options': [
+                { 'model': 'paragraph', 'title': 'Paragraph', 'class': 'ck-heading_paragraph' },
+                { 'model': 'heading1', 'view': 'h1', 'title': 'Heading 1', 'class': 'ck-heading_heading1' },
+                { 'model': 'heading2', 'view': 'h2', 'title': 'Heading 2', 'class': 'ck-heading_heading2' },
+                { 'model': 'heading3', 'view': 'h3', 'title': 'Heading 3', 'class': 'ck-heading_heading3' }
+            ]
+        }
+    },
+    'list': {
+        'properties': {
+            'styles': 'true',
+            'startIndex': 'true',
+            'reversed': 'true',
+        }
+    }
+}
+
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
@@ -60,6 +129,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'compressor',
+    'django_ckeditor_5',
     'blog_app.apps.BlogAppConfig'
 ]
 
