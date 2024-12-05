e73e4976fdd5ecc8c1e3eab4dcb6cd3601d9d6fc
Oscar Luntha <lunthaoscar@gmail.com>
2024-12-05 16:29:57 +0200

    ensure that a user cannot like the same post more than once

--- blog-app-project/blog_app/models.py
+++ blog-app-project/blog_app/models.py
@@ -27,7 +27,11 @@ class Comment(models.Model):
     
 class Like(models.Model):
     post_id = models.ForeignKey(BlogPost,on_delete=models.CASCADE)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     
+    class Meta:
+        constraints= [ models.UniqueConstraint(fields = ['user','post_id'], name='unique_user_blogpost_like')]
+    def __str__(self) -> str:
+        return f"{self.user.username} liked {self.blog_post.title}"
     
         
