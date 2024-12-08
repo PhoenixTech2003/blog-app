c4247b484b292610c2ad31753f852403a1bbe927
Oscar Luntha <lunthaoscar@gmail.com>
2024-12-08 11:10:06 +0200

    include a log in path

--- blog-app-project/blogging_site/urls.py
+++ blog-app-project/blogging_site/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("v1/blog-app/", include("blog_app.urls")),
     path('admin/', admin.site.urls),
-    
+    path('accounts/',include('django.contrib.auth.urls'))
 ]
 
 
