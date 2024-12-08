efd3cd060062b79322f056cf04a36925e5d65753
Oscar Luntha <lunthaoscar@gmail.com>
2024-12-08 11:13:48 +0200

    include a register path

--- blog-app-project/blog_app/urls.py
+++ blog-app-project/blog_app/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
    path("<int:postId>", views.blog_post, name="post")
+   path("signup/",views.signup,name = 'signup')
 ]
