a646c68badb9474b5e0253ce3b7488c773b4988a
Oscar Luntha <lunthaoscar@gmail.com>
2024-12-08 15:24:59 +0200

    add a path for liking a post

--- blog-app-project/blog_app/urls.py
+++ blog-app-project/blog_app/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-   path("<int:postId>", views.blog_post, name="post")
+   path("<int:postId>/like/", views.like_blog_post,name='like_blog_post'),
+   path("<int:postId>/", views.blog_post, name='post'),
    path("signup/",views.signup,name = 'signup')
 ]
