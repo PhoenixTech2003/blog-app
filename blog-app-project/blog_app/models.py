a95ad6370adb16acd063a914622c93e6ef870751
Oscar Luntha <lunthaoscar@gmail.com>
2024-12-08 15:22:05 +0200

    add a total_likes method for tracking the # of likes per post

--- blog-app-project/blog_app/models.py
+++ blog-app-project/blog_app/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth import get_user_model
 from django.db.models import Count
 from django_ckeditor_5.fields import CKEditor5Field
 User = get_user_model()
+from django.contrib.auth.models import User
 
 # Create your models here.
 class BlogPost(models.Model):
@@ -12,12 +13,15 @@ class BlogPost(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     author_id = models.ForeignKey(User,on_delete=models.CASCADE)
     photo = models.ImageField(upload_to='blog_post_images', null=True)
-    
+    likes = models.ManyToManyField(User, related_name='liked_posts', blank=True)
     def __str__(self):
         return self.title
     
+    def total_likes(self):
+        return self.likes.count()
+    
     def top_liked_posts(self):
-        return self.objects.annotate(like_count=Count('like')).order_by('-like_count')[:5]
+        return self.objects.annotate(like_count=Count('likes')).order_by('-like_count')[:5]
     
 class Comment(models.Model):
     content =  models.TextField()
@@ -28,10 +32,3 @@ class Comment(models.Model):
 class Like(models.Model):
     post_id = models.ForeignKey(BlogPost,on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    
-    class Meta:
-        constraints= [ models.UniqueConstraint(fields = ['user','post_id'], name='unique_user_blogpost_like')]
-    def __str__(self) -> str:
-        return f"{self.user.username} liked {self.blog_post.title}"
-    
-        
